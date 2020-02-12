d286f24834139580aaf87e21501a45f5d2ecc233
norio c <norionorioc@gmail.com>
2020-02-12 03:33:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  norio= ["one", "two", "three", "four"]
+  famous_cats = ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
